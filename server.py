020cd4f0e7559aa957ce55714a289f2e38ac5f97
MathieuMorrissette <mathieumorrissette@gmail.com>
2019-06-22 12:27:17 -0400

    server

--- server.py
+++ server.py
@@ -18,18 +18,15 @@ async def hello(websocket, path):
             senders.append(websocket)
         try:
             while True:
-                try:
-                    print("waiting to receive data...")
-                    client_data = await websocket.recv()
-                    print("received data : " + client_data)
+                print("waiting to receive data...")
+                client_data = await websocket.recv()
+                print("received data : " + client_data)
 
-                    if(client_data.startswith("KEY|")):
-                        key = client_data[4:]
-                        print("sending " + key)
-                        await broadcast_key(key)
-                        print("done sending")
-                except Exception as e:
-                    print(str(e))
+                if(client_data.startswith("KEY|")):
+                    key = client_data[4:]
+                    print("sending " + key)
+                    await broadcast_key(key)
+                    print("done sending")
 
         except Exception as e:
             print(str(e))
@@ -48,7 +45,7 @@ async def broadcast_key(key):
         
 
 # ping_interval None -> disables timeout
-start_server = websockets.serve(hello, '192.168.4.148', 8766, ping_timeout=0)
+start_server = websockets.serve(hello, '192.168.4.148', 8766, ping_interval=None)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
