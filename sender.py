3d0c18f7d2cd5ab69d0b65e3a228646cabcef94f
Mathieu Morrissette <mathieumorrissette@gmail.com>
2019-06-23 17:33:56 -0400

    fix bug

--- sender.py
+++ sender.py
@@ -18,14 +18,28 @@ async def hello():
         while(True):
             data_to_send = input("> ")
 
-            await websocket.send(data_to_send)
+            if(data_to_send.startswith("PUT|")):
+                split = data_to_send.split("|")
+                source = split[1]
+                destination = split[2]
+
+                f = open(source, "rb")
+                data = base64.b64encode(f.read())
+                await websocket.send("PUT|" + destination + "|" + data)
+
+                # freeze until transfer done
+                await websocket.recv()
             
             if(data_to_send.startswith("CMD|")):
+                await websocket.send(data_to_send)
                 print(await websocket.recv())
 
             if(data_to_send.startswith("SCREEN|")):
+                await websocket.send(data_to_send)
                 img = b64_2_img(bytes(await websocket.recv()))
                 img.show()
+            
+
             
 
 asyncio.get_event_loop().run_until_complete(hello())
